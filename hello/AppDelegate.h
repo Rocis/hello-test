2d50db7aa52e619ce9dac3321c1fa4556bb1139f
Rocis <1150823886@qq.com>
2014-11-24 11:32:06 +0800

    Initial Commit

---
+++ hello/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hello
+//
+//  Created by PIMEI_IMac on 14-11-24.
+//  Copyright (c) 2014年 PIMEI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
